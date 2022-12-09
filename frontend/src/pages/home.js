cb29b01820bfc5dab19cd0b2d58885fd4418c5cd
Ugege Daniel <ugege62@gmail.com>
2022-12-09 11:37:34 +0100

    proptypes

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,6 +1,6 @@
+import { useState } from 'react'
 import PropTypes from 'prop-types';
 import { Box } from '@material-ui/core'
-import { useState } from 'react'
 import { ModalComponent, Form, Hero } from '../components'
 
 const Home = ({ setNotification, setStudent }) => {
